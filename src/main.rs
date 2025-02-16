fec2ffa3a7bfdadd0054156975628010b7d293d3
vonacht <vonachtf80@gmail.com>
2025-02-16 12:44:25 +0100

    moved everything to trait methods

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,160 @@ use std::str::FromStr;
 
 type CD2ifierResult<T> = Result<T, Box<dyn Error>>;
 
+struct DiffContainer {
+    new: JsonValue,
+    original: JsonValue,
+}
+
+trait CDObject {
+    fn copy_field_if_exists(self, field: &str, err_msg: Option<&str>) -> Self;
+    fn build_resupply_module(self) -> Self;
+    fn build_enemies_module(self, translation_data: &JsonValue) -> Self;
+    fn build_top_modules(self, translation_data: &JsonValue) -> Self;
+    fn special_renames(self) -> Self;
+}
+
+impl CDObject for DiffContainer {
+    fn copy_field_if_exists(self, field: &str, err_msg: Option<&str>) -> Self {
+        if !self.original[field].is_null() {
+            let mut new = self.new.clone();
+            new[field] = self.original[field].clone();
+            DiffContainer {
+                new,
+                original: self.original,
+            }
+        } else {
+            if let Some(msg) = err_msg {
+                println!("Field {} was missing. {}", field, msg);
+            }
+            self
+        }
+    }
+    fn build_resupply_module(self) -> Self {
+        // Resupply module. Copy the cost if StartingNitra is 0 or missing, otherwise add
+        // the corresponding nitra mutator:
+        let mut new = self.new.clone();
+        let original_resupply_cost: f64 =
+            if !self.original["ResupplyCost"].is_null() && self.original["ResupplyCost"] != 80 {
+                self.original["ResupplyCost"].as_f64().unwrap()
+            } else {
+                80.00
+            };
+        if self.original["StartingNitra"].is_null() || self.original["StartingNitra"] == 0 {
+            new["Resupply"]["Cost"] = original_resupply_cost.into();
+        } else {
+            let first_resupply: f64 = if self.original["StartingNitra"].as_f64().unwrap()
+                >= original_resupply_cost
+            {
+                println!("Warning: This script does not support a StartingNitra higher than the resupply Cost for now.");
+                println!("It will set up the first supply free.");
+                0.0
+            } else {
+                original_resupply_cost - self.original["StartingNitra"].as_f64().unwrap()
+            };
+
+            new["Resupply"]["Cost"] = object! {
+                "Mutate": "IfFloat",
+                "Value": {
+                  "Mutate": "ResuppliesCalled"
+                },
+                "==": 0,
+                "Then": first_resupply,
+                "Else": original_resupply_cost
+            }
+        }
+        DiffContainer {
+            new,
+            original: self.original,
+        }
+    }
+    fn build_enemies_module(self, translation_data: &JsonValue) -> Self {
+        // Enemies module, copy as-is but fix the old pawn stats and remove deprecated fields:
+        let mut new = self.new.clone();
+        if !self.original["EnemyDescriptors"].is_null() {
+            new["EnemiesNoSync"] = self.original["EnemyDescriptors"].clone();
+            // Fix pawn stats:
+            for (enemy, controls) in new["EnemiesNoSync"].entries_mut() {
+                if !controls["PawnStats"].is_null() {
+                    let pawn_stats = controls.remove("PawnStats");
+                    translate_pawn_stats(controls, &pawn_stats, &translation_data["PAWN_STATS"]);
+                }
+                // Remove deprecated fields:
+                for (field, _) in self.original["EnemyDescriptors"][enemy].entries() {
+                    if !translation_data["VALID_ENEMY_CONTROLS"].contains(field)
+                        && field != "PawnStats"
+                    {
+                        println!(
+                            "Deprecated enemy control: {} in {}. Skipping.",
+                            field, enemy
+                        );
+                        controls.remove(field);
+                    }
+                }
+            }
+        }
+        DiffContainer {
+            new,
+            original: self.original,
+        }
+    }
+    fn build_top_modules(self, top_modules_map: &JsonValue) -> Self {
+        let mut new = self.new.clone();
+        for (original_key, original_value) in self.original.entries() {
+            if let Some(field_status) = top_modules_map[original_key].as_str() {
+                match FieldStatus::from_str(field_status).unwrap() {
+                    FieldStatus::Valid(top_module) => {
+                        // This if block is trying to detect fields that have weights, since CD2 removes the
+                        // "range" part of the bins:
+                        if original_value.is_array()
+                            && !original_value.is_empty()
+                            && !original_value[0]["weight"].is_null()
+                        {
+                            let mut removed_ranges_arr = json::JsonValue::new_array();
+                            for bin in original_value.members() {
+                                removed_ranges_arr
+                                    .push(object! {
+                                        "weight": bin["weight"].clone(),
+                                        "min": bin["range"]["min"].clone(),
+                                        "max": bin["range"]["max"].clone()
+                                    })
+                                    .unwrap();
+                            }
+                            new[top_module][original_key] = removed_ranges_arr;
+                        } else {
+                            new[top_module][original_key] = original_value.clone();
+                        }
+                    }
+                    FieldStatus::Deprecated => {
+                        println!("Deprecated field: {}. Skipping.", original_key);
+                    }
+                    FieldStatus::Ignored => (),
+                }
+            } else {
+                println!("Unsupported field: {}. Please open an issue.", original_key);
+            }
+        }
+        DiffContainer {
+            new,
+            original: self.original,
+        }
+    }
+    fn special_renames(self) -> Self {
+        let mut new = self.new.clone();
+        // Here we add the BaseHazard field, defaults to HAzard 5 for explicitness:
+        new["DifficultySetting"]["BaseHazard"] = "Hazard 5".into();
+        // Change the name of StationaryEnemies, which in CD2 changed name to StationaryPool:
+        let stationary_enemies = new["Pools"].remove("StationaryEnemies");
+        if !stationary_enemies.is_null() {
+            new["Pools"]["StationaryPool"] = stationary_enemies
+        }
+        DiffContainer {
+            new,
+            original: self.original,
+        }
+    }
+}
+
 enum FieldStatus {
     Deprecated,
     Ignored,
@@ -42,170 +196,6 @@ fn main() {
     }
 }
 
-fn parse_json(path: &str) -> JsonValue {
-    let file_string = fs::read_to_string(path).unwrap_or_else(|err| {
-        panic!(
-            "Something went wrong when reading the file in {}: {}",
-            path, err
-        )
-    });
-    json::parse(&file_string).unwrap_or_else(|err| {
-        panic!(
-            "The JSON parser couldn't parse {}: {}. Is it a proper JSON?",
-            path, err
-        )
-    })
-}
-
-fn run(args: &Args) -> CD2ifierResult<()> {
-    // Open the files containing CD1 to CD2 translation data:
-    let translation_data = parse_json("src/cd2-modules.json");
-    // Open the original difficulty file:
-    let original_diff = parse_json(&args.source_file);
-
-    let mut target_diff = json::JsonValue::new_object();
-
-    // Name and description, copy as-is:
-    if !original_diff["Description"].is_null() {
-        target_diff["Description"] = original_diff["Description"].clone();
-    } else {
-        println!("The original file doesn't have a description, skipping.")
-    }
-    if !original_diff["Name"].is_null() {
-        target_diff["Name"] = original_diff["Name"].clone();
-    } else {
-        println!("The original file doesn't have a name, skipping. It is recommended to add one.")
-    }
-
-    // Resupply module. Copy the cost if StartingNitra is 0 or missing, otherwise add
-    // the corresponding nitra mutator:
-    let original_resupply_cost: f64 =
-        if !original_diff["ResupplyCost"].is_null() && original_diff["ResupplyCost"] != 80 {
-            original_diff["ResupplyCost"].as_f64().unwrap()
-        } else {
-            80.00
-        };
-    if original_diff["StartingNitra"].is_null() || original_diff["StartingNitra"] == 0 {
-        target_diff["Resupply"]["Cost"] = original_resupply_cost.into();
-    } else {
-        let first_resupply: f64 = if original_diff["StartingNitra"].as_f64().unwrap()
-            >= original_resupply_cost
-        {
-            println!("Warning: This script does not support a StartingNitra higher than the resupply Cost for now.");
-            println!("It will set up the first supply free.");
-            0.0
-        } else {
-            original_resupply_cost - original_diff["StartingNitra"].as_f64().unwrap()
-        };
-
-        target_diff["Resupply"]["Cost"] = object! {
-            "Mutate": "IfFloat",
-            "Value": {
-              "Mutate": "ResuppliesCalled"
-            },
-            "==": 0,
-            "Then": first_resupply,
-            "Else": original_resupply_cost
-        }
-    }
-
-    // Loop over the original fields and translate them into the new top level modules
-    // as specified in cd2-modules.json:
-    for (key, val) in original_diff.entries() {
-        build_top_module(&translation_data["TOP_MODULES"], &mut target_diff, key, val);
-    }
-    // Add the BaseHazard field, which is new in CD2, default to Hazard 5 for explicitness:
-    target_diff["DifficultySetting"]["BaseHazard"] = "Hazard 5".into();
-    // Change the name of StationaryEnemies, which is StationaryPool in CD2:
-    let stationary_enemies = target_diff["Pools"].remove("StationaryEnemies");
-    if !stationary_enemies.is_null() {
-        target_diff["Pools"]["StationaryPool"] = stationary_enemies
-    }
-    // Enemies module, copy as-is but fix the old pawn stats and remove deprecated fields:
-    if !original_diff["EnemyDescriptors"].is_null() {
-        target_diff["EnemiesNoSync"] = original_diff["EnemyDescriptors"].clone();
-        // Fix pawn stats:
-        for (enemy, controls) in target_diff["EnemiesNoSync"].entries_mut() {
-            if !controls["PawnStats"].is_null() {
-                let pawn_stats = controls.remove("PawnStats");
-                translate_pawn_stats(controls, &pawn_stats, &translation_data["PAWN_STATS"]);
-            }
-            // Remove deprecated fields:
-            for (field, _) in original_diff["EnemyDescriptors"][enemy].entries() {
-                if !translation_data["VALID_ENEMY_CONTROLS"].contains(field) && field != "PawnStats"
-                {
-                    println!(
-                        "Deprecated enemy control: {} in {}. Skipping.",
-                        field, enemy
-                    );
-                    controls.remove(field);
-                }
-            }
-        }
-    }
-    // Escort module, copy as-is:
-    if !original_diff["EscortMule"].is_null() {
-        target_diff["EscortMule"] = original_diff["EscortMule"].clone();
-    }
-
-    fs::write(
-        &args.target_file,
-        if args.dont_pretty_print {
-            json::stringify(target_diff)
-        } else {
-            json::stringify_pretty(target_diff, 4)
-        },
-    )
-    .unwrap_or_else(|err| {
-        panic!(
-            "There was a problem when writing to the final file {}, {}",
-            &args.target_file, err
-        )
-    });
-
-    Ok(())
-}
-
-fn build_top_module(
-    module_map: &JsonValue,
-    new_file: &mut JsonValue,
-    original_key: &str,
-    new_value: &JsonValue,
-) {
-    if let Some(field_status) = module_map[original_key].as_str() {
-        match FieldStatus::from_str(field_status).unwrap() {
-            FieldStatus::Valid(top_module) => {
-                // This if block is trying to detect fields that have weights, since CD2 removes the
-                // "range" part of the bins:
-                if new_value.is_array()
-                    && !new_value.is_empty()
-                    && !new_value[0]["weight"].is_null()
-                {
-                    let mut removed_ranges_arr = json::JsonValue::new_array();
-                    for bin in new_value.members() {
-                        removed_ranges_arr
-                            .push(object! {
-                                "weight": bin["weight"].clone(),
-                                "min": bin["range"]["min"].clone(),
-                                "max": bin["range"]["max"].clone()
-                            })
-                            .unwrap();
-                    }
-                    new_file[top_module][original_key] = removed_ranges_arr;
-                } else {
-                    new_file[top_module][original_key] = new_value.clone();
-                }
-            }
-            FieldStatus::Deprecated => {
-                println!("Deprecated field: {}. Skipping.", original_key);
-            }
-            FieldStatus::Ignored => (),
-        }
-    } else {
-        println!("Unsupported field: {}. Please open an issue.", original_key);
-    }
-}
-
 fn translate_pawn_stats(
     controls: &mut JsonValue,
     pawn_stats: &JsonValue,
@@ -225,4 +215,55 @@ fn translate_pawn_stats(
             println!("Unsupported pawn stat: {}. Please open an issue.", stat);
         }
     }
+}
+
+fn parse_json(path: &str) -> JsonValue {
+    let file_string = fs::read_to_string(path).unwrap_or_else(|err| {
+        panic!(
+            "Something went wrong when reading the file in {}: {}",
+            path, err
+        )
+    });
+    json::parse(&file_string).unwrap_or_else(|err| {
+        panic!(
+            "The JSON parser couldn't parse {}: {}. Is it a proper JSON?",
+            path, err
+        )
+    })
+}
+
+fn run(args: &Args) -> CD2ifierResult<()> {
+    // Open the file containing CD1 to CD2 translation data:
+    let translation_data = parse_json("src/cd2-modules.json");
+    let result = DiffContainer {
+        new: json::JsonValue::new_object(),
+        original: parse_json(&args.source_file),
+    }
+    .copy_field_if_exists("Name", "It is recommended to add a Name".into())
+    .copy_field_if_exists(
+        "Description",
+        "It is recommended to add a Description".into(),
+    )
+    .build_resupply_module()
+    .build_top_modules(&translation_data["TOP_MODULES"])
+    .build_enemies_module(&translation_data)
+    .copy_field_if_exists("EscortMule", None)
+    .special_renames();
+
+    fs::write(
+        &args.target_file,
+        if args.dont_pretty_print {
+            json::stringify(result.new)
+        } else {
+            json::stringify_pretty(result.new, 4)
+        },
+    )
+    .unwrap_or_else(|err| {
+        panic!(
+            "There was a problem when writing to the final file {}, {}",
+            &args.target_file, err
+        )
+    });
+
+    Ok(())
 }
